d6d50126b5ad974e4a48e9755042366e63881d2c
fgonzalezr1998 <fergonzaramos@yahoo.es>
2021-01-11 19:45:08 +0100

    [p2] memory finished

--- practices/p2/main.go
+++ practices/p2/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"sync"
 	"os"
+	"fmt"
 	"./toy_factory"
 )
 
-func wait_for_end(time_to_deal_ch chan struct{}, mutex * sync.RWMutex) {
+func wait_for_end(time_to_deal_ch chan struct{}) {
 	for {
 		select {
 			case <- time_to_deal_ch:
+				fmt.Println("[INFO] TIME TO DEAL!")
 				os.Exit(0)
 		}
 	}
@@ -22,5 +24,5 @@ func main() {
 
 	toy_factory.Init(&mutex)	// Initialize the Toy Factory
 
-	wait_for_end(toy_factory.Time_to_deal_ch, &mutex)
+	wait_for_end(toy_factory.Time_to_deal_ch)
 }
